5f7688fec9594116c3110164aaf9fc87b51409f6
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 14:49:58 +0400

    Delete 02-Type-Annotations-With-Multidimensional-Arrays directory

--- 02-Type-Annotations-With-Multidimensional-Arrays/dist/02-Type-Annotations-With-Multidimensional-Arrays.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*************************************************
- * TYPE ANNOTATIONS WITH MULTIDIMENSIONAL ARRAYS *
- *************************************************/
-Object.defineProperty(exports, "__esModule", { value: true });
-let arrayOne = [1, 2, 3, 4, 5];
-let arrayTwo = ["A", "B", "C"];
-let arrayThree = [1, 2, 3, 4, "A", "B", "C"];
-//# sourceMappingURL=02-Type-Annotations-With-Multidimensional-Arrays.js.map
--- 02-Type-Annotations-With-Multidimensional-Arrays/src/02-Type-Annotations-With-Multidimensional-Arrays.ts
+++
@@ -1,7 +0,0 @@
-/*************************************************
- * TYPE ANNOTATIONS WITH MULTIDIMENSIONAL ARRAYS *
- *************************************************/
-
-let arrayOne: number[] = [1, 2, 3, 4, 5];
-let arrayTwo: string[] = ["A", "B", "C"];
-let arrayThree: (string | number)[] = [1, 2, 3, 4, "A", "B", "C"];
