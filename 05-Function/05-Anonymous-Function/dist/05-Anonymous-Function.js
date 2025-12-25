d347abbd26d0a251e13e359404612acdd09f199e
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 13:22:13 +0400

    Add files via upload

---
+++ 05-Function/05-Anonymous-Function/dist/05-Anonymous-Function.js
@@ -0,0 +1,10 @@
+"use strict";
+/**********************
+ * ANONYMOUS FUNCTION *
+ **********************/
+Object.defineProperty(exports, "__esModule", { value: true });
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(10, 20)); // Output: 30
+//# sourceMappingURL=05-Anonymous-Function.js.map
