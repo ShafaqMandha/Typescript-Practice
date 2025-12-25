a7ddeea757c4012e3bd41e046d0b57a0710ce1c6
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 11:25:28 +0400

    Add files via upload

---
+++ 03-Types/02-Dynamic-Type-Determination/dist/02-Dynamic-Type-Determination.js
@@ -0,0 +1,8 @@
+"use strict";
+/******************************
+ * DYNAMIC TYPE DETERMINATION *
+ ******************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+let greeting = "Hello, world!";
+console.log(greeting);
+//# sourceMappingURL=02-Dynamic-Type-Determination.js.map
