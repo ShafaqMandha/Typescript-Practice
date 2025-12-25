6f57cb34d0bc5ef2df32fa64767733b96590dc49
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 14:32:51 +0400

    Add files via upload

---
+++ 07-Array/01-Type-Annotations-With-Arrays/dist/01-Type-Annotations-With-Arrays.js
@@ -0,0 +1,12 @@
+"use strict";
+/********************************
+ * TYPE ANNOTATIONS WITH ARRAYS *
+ ********************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+// Annotating an array of strings
+let fruits = ["apple", "banana", "cherry"];
+// Using a loop to process each element in the array
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Fruit: ${fruits[i].toUpperCase()}`);
+}
+//# sourceMappingURL=01-Type-Annotations-With-Arrays.js.map
