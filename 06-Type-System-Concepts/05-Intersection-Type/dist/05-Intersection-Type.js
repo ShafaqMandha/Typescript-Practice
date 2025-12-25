477fa100c53e1791e1a30c91b0828e9072ec7261
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 13:51:14 +0400

    Add files via upload

---
+++ 06-Type-System-Concepts/05-Intersection-Type/dist/05-Intersection-Type.js
@@ -0,0 +1,12 @@
+"use strict";
+/*********************
+ * INTERSECTION TYPE *
+ *********************/
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "John Doe",
+    age: 30,
+    address: "123 Main St",
+    phone: "123-456-7890",
+};
+//# sourceMappingURL=05-Intersection-Type.js.map
