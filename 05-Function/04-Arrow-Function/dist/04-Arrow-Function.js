7b5fcccd3b7d15f0664784d253586ecc5c086658
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-25 13:19:06 +0400

    Add files via upload

---
+++ 05-Function/04-Arrow-Function/dist/04-Arrow-Function.js
@@ -0,0 +1,8 @@
+"use strict";
+/******************
+ * ARROW FUNCTION *
+ ******************/
+Object.defineProperty(exports, "__esModule", { value: true });
+const addWithArrow = (num1, num2) => num1 + num2;
+console.log(addWithArrow(10, 20)); // Output: 30
+//# sourceMappingURL=04-Arrow-Function.js.map
