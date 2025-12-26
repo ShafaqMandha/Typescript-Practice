dab6a4822815f872f0bef0a3a20ba4a02a48b39a
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 12:11:27 +0400

    Add files via upload

---
+++ 10-Interface/05-Interface-VS-Type-Aliases/dist/05-Interface-VS-Type-Aliases.js
@@ -0,0 +1,22 @@
+"use strict";
+/******************************
+ * INTERFACE VS. TYPE ALIASES *
+ ******************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+class Labrador {
+    type = "dog";
+    bark() {
+        console.log("Woof!");
+    }
+}
+const labrador = {
+    type: "monkey",
+    bark() {
+        console.log("Woof!");
+    },
+};
+const myCar = {
+    brand: "Toyota",
+    model: "Camry",
+};
+//# sourceMappingURL=05-Interface-VS-Type-Aliases.js.map
