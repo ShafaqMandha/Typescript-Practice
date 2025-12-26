7188b7a369cd4859a7b8d234c390b93cde35238f
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 15:01:37 +0400

    Add files via upload

---
+++ 12-Generics/01-Generics-Intro/dist/01-Generics-Intro.js
@@ -0,0 +1,19 @@
+"use strict";
+/******************
+ * GENERICS INTRO *
+ ******************/
+Object.defineProperty(exports, "__esModule", { value: true });
+// A generic function that returns the input value as is
+function returnType(val) {
+    return val;
+}
+// Usage of the generic function with different types
+const numValue = returnType(100);
+const strValue = returnType("Elzero");
+const boolValue = returnType(true);
+const arrValue = returnType([1, 2, 3, 4]);
+console.log(`Number Value: ${numValue}`);
+console.log(`String Value: ${strValue}`);
+console.log(`Boolean Value: ${boolValue}`);
+console.log(`Array Value: ${arrValue}`);
+//# sourceMappingURL=01-Generics-Intro.js.map
