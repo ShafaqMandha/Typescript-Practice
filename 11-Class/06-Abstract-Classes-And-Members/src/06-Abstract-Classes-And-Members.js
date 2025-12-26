59231ba5e044bfa8c232f9bf35e9b985fff970d8
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 13:38:59 +0400

    Add files via upload

---
+++ 11-Class/06-Abstract-Classes-And-Members/src/06-Abstract-Classes-And-Members.js
@@ -0,0 +1,30 @@
+"use strict";
+/********************************
+ * ABSTRACT CLASSES AND MEMBERS *
+ ********************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+class AbstractItem {
+    id;
+    name;
+    static nextId = 1;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    static generateNextId() {
+        return AbstractItem.nextId++;
+    }
+}
+class Item extends AbstractItem {
+    constructor(id, name) {
+        super(id, name);
+    }
+    getItemInfo() {
+        return `ID: ${this.id}, Name: ${this.name}`;
+    }
+}
+const item1 = new Item(AbstractItem.generateNextId(), "Widget");
+const item2 = new Item(AbstractItem.generateNextId(), "Gadget");
+console.log(item1.getItemInfo()); // ID: 1, Name: Widget
+console.log(item2.getItemInfo()); // ID: 2, Name: Gadget
+//# sourceMappingURL=06-Abstract-Classes-And-Members.js.map
