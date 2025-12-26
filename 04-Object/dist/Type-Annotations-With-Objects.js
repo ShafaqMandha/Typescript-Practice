802d004ca0e12f3d9f8d019ca30e49cc0d1b5d1f
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 12:10:52 +0400

    Add files via upload

---
+++ 04-Object/dist/Type-Annotations-With-Objects.js
@@ -0,0 +1,26 @@
+"use strict";
+/*********************************
+ * TYPE ANNOTATIONS WITH OBJECTS *
+ *********************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+let personExOne;
+personExOne = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+// And there is yet another method
+let personExTwo = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+//# sourceMappingURL=Type-Annotations-With-Objects.js.map
