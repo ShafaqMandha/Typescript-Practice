c612e81b24e57bbc29efd4b94d54f588320893ea
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 11:50:20 +0400

    Add files via upload

---
+++ 10-Interface/01-Interface-Intro/dist/01-Interface-Intro.js
@@ -0,0 +1,10 @@
+"use strict";
+/*******************
+ * INTERFACE INTRO *
+ *******************/
+Object.defineProperty(exports, "__esModule", { value: true });
+const John = {
+    name: "John",
+    age: 30,
+};
+//# sourceMappingURL=01-Interface-Intro.js.map
