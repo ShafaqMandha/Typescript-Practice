7fc0e9b1d4af3871a98ba05610cd7c6b70589779
Shafaq Mandha <135320827+ShafaqMandha@users.noreply.github.com>
2025-12-26 12:05:45 +0400

    Add files via upload

---
+++ 10-Interface/04-HTMLImageElement/dist/04-HTMLImageElement.js
@@ -0,0 +1,18 @@
+"use strict";
+/********************
+ * HTMLIMAGEELEMENT *
+ ********************/
+Object.defineProperty(exports, "__esModule", { value: true });
+// This example is for educational purposes, but it is not going to work. 
+const imgElement = document.createElement("img");
+imgElement.src = "image.jpg";
+imgElement.alt = "My Image";
+if (imgElement.complete) {
+    console.log(`Image dimensions: ${imgElement.naturalWidth}x${imgElement.naturalHeight}`);
+}
+else {
+    imgElement.addEventListener("load", () => {
+        console.log(`Image dimensions: ${imgElement.naturalWidth}x${imgElement.naturalHeight}`);
+    });
+}
+//# sourceMappingURL=04-HTMLImageElement.js.map
